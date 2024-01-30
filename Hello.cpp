1cd6deff9418c585fded23c31895698305a95c01
MelvinIsTheBug <157755882+MelvinIsTheBug@users.noreply.github.com>
2024-01-29 21:56:43 -0800

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+/*** 
+{Learning c++ or something}
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    printf("Hello World Again!\n");
+    return 0;
+}
